0da449255b3575ba0895c33dbc59a5a522e1c53f
HBK0077 <128217372+HBK0077@users.noreply.github.com>
2023-04-25 21:02:32 +0530

    Displayed in webpage

--- first-app.js
+++ first-app.js
@@ -1,7 +1,8 @@
 console.log("Hello World");
+document.getElementById("demo").innerHTML="Hello world";
 
 // writing the text to a file.
 
-const fs = require("fs");
+// const fs = require("fs");
 
-fs.writeFileSync("hello.txt", "hello from node.js");
+// fs.writeFileSync("hello.txt", "hello from node.js");
