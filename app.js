4475738233d59aa46c3c4514df0a15f36c6cb362
HBK0077 <128217372+HBK0077@users.noreply.github.com>
2023-05-08 14:42:19 +0530

    Middlewares committed with parseBody

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
-
-
 const express = require('express');
+
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log("Inside middleware");
-    next();
+//body parsing should be done at the beginning of the middlewares.
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use("/add-product",(req,res,next)=>{
+    //console.log("Inside middleware");
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>');
 });
 
-app.use((req,res,next)=>{
-    console.log("Inside another middleware");
-    res.send({key1:value});
+app.use("/product",(req,res,next)=>{
+    console.log(req.body);
+    res.redirect("/");
+});
+
+app.use("/",(req,res,next)=>{
+    //console.log("Inside another middleware");
+    res.send("<h1>Hello from Express JS</h1>");
 });
 
 app.listen(5000);
