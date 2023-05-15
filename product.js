2dcd92857ca42185981b1f677ee17f6cf6f7f23f
HBK0077 <128217372+HBK0077@users.noreply.github.com>
2023-05-15 12:44:12 +0530

    dynamicRoutes displaying product details committed

--- product.js
+++ product.js
@@ -1,44 +1,48 @@
-const products = [];
-const { json } = require('body-parser');
 const fs = require('fs');
-const path= require('path');
+const path = require('path');
+
 const p = path.join(
-    __dirname,
-    '../',
-    'data',
-    'products.json'
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
 );
 
-const getProductsFromFile = (cb)=>{
-     //const fileData = fs.readFile("products.txt");
-    
-    fs.readFile(p, (err,fileContent)=>{
-        if(err){
-            cb([]);
-        }else{
-        cb(JSON.parse(fileContent));
-        }
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    this.id = Math.random().toString();
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
     });
-}
+  }
 
-module.exports = class Product{
-    constructor(title){
-        this.title = title;
-    }
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
 
-    save(){
-        // products.push(this);
-        // fs.writeFile("products.txt",products);
-        getProductsFromFile(products =>{
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products),(err)=>{
-                console.log(err);
-            });
-        });
-
-    }
-
-    static fetchAll(cb){
-       getProductsFromFile(cb);
-    }
-}
+  static findById(id, cb){
+    getProductsFromFile(products =>{
+      const product = products.find(p => p.id === id)
+      cb(product);
+    });
+  }
+};
