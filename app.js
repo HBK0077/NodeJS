67c22e6c147889427768d3b144e4af1e699a00e3
HBK0077 <128217372+HBK0077@users.noreply.github.com>
2023-05-17 12:58:52 +0530

    SQL connection with data retrieving committed.

--- app.js
+++ app.js
@@ -4,19 +4,28 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+db.execute('select * from products')
+.then(result=>{
+    console.log(result);
+})
+.catch(err=>{
+    console.log(err);
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
