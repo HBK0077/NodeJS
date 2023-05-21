ed66ceb347ef73a6b2b46278f2e0e6472c500cfc
HBK0077 <128217372+HBK0077@users.noreply.github.com>
2023-05-21 17:13:33 +0530

    Relationships (associationS) committed

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
-const express=require("express")
-const cors=require("cors")
-const bodyparser=require("body-parser")
-const sequelize=require("./util/database")
+const path = require('path');
 
-const expenseDetails=require("./routes/expenses")
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const app=express();
-app.use(cors());
-app.use(bodyparser.json())
+const errorController = require('./controllers/error');
+const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
-app.use(expenseDetails)
+const app = express();
 
-sequelize.sync().then(()=>{
-    app.listen(2500)
-})
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
+sequelize
+  
+  .sync()
+  .then(result => {
+    return User.findById(1);
+    // console.log(result);
+    //app.listen(3000);
+  }).then(user=>{
+    if(!user){
+      return User.create({name: "Max", email: "test@test.com" });
+    }
+    return user;
+  })
+  .then(user=>{
+    console.log(user);
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
